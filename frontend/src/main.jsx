8556b94ef1cd238b5204bb4f023d7833cd08bb94
ShagthiSarweshG <shagthisarweshg@gmail.com>
2025-04-21 01:59:07 +0530

    project completed

---
+++ frontend/src/main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "./components/ui/provider";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
